29ae50327709572585244dc0f010c14bae724983
Cbayona <vendettacb@gmail.com>
2019-07-31 11:49:09 -0500

    commit inicial

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => (
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="/" alt=""/>
+                </figure>
+            </div>
+            <div className="About-Name">
+                <h3>Camilo Bayona Orduz</h3>
+            </div>
+            <div className="About-profession">
+                <p>Fullstack Developer</p>
+            </div>
+            <div className="About-desc">
+                <p>Soy desarrollador web y me encantan las truchas</p>
+            </div>
+            <div className="About-location">
+                <p>Bogotá D.C., Colombia.</p>
+            </div>
+            <div className="About-social">
+                Social
+            </div>
+        </div>
+    </div>
+);
+
+export default About;
