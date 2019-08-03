a8831197cc3ee4febf810137728b9cee36133cab
Cbayona <vendettacb@gmail.com>
2019-08-03 12:53:38 -0500

    Aplicacion de estilos con Styled Components

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import H2Styled from '../styled/H2Styled';
+import H3Styled from '../styled/H3Styled';
+import PStyled from '../styled/PStyled';
+
 
 const Certificates = props => (
     <div className="Certificates">
+        <H2Styled name="Certificates"/>
         <div className="Certificates-container">
         {props.data.map((cert, index) => (
             <div className="Certificates-item" key={`Cert-${index}`}>
-                <h3>{cert.name} {cert.institution}
+                <H3Styled>{cert.name} {cert.institution}
                 <span>{cert.date}</span>
-                </h3>
-                <p>{cert.description}</p>
+                </H3Styled>
+                <PStyled name={cert.description}/>
             </div>
         ))}
         </div>
